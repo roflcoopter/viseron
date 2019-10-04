b7c8ff4dd46c62333a871c22bebf7389232d478f
Jesper Nilsson <jespernilsson93@hotmail.se>
2019-10-04 12:13:32 +0000

    black

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,7 +1,12 @@
 def scale_bounding_box(image_size, bounding_box, target_size):
+    """Scales a bounding box to target image size"""
     x1p = bounding_box[0] / image_size[0]
     y1p = bounding_box[1] / image_size[1]
     x2p = bounding_box[2] / image_size[0]
     y2p = bounding_box[3] / image_size[1]
-    return (x1p * target_size[0], y1p * target_size[1],
-            x2p * target_size[0], y2p * target_size[1])
+    return (
+        x1p * target_size[0],
+        y1p * target_size[1],
+        x2p * target_size[0],
+        y2p * target_size[1],
+    )
