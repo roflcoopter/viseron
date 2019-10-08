9659087959485fab1711ed00e501c0518b95633f
Jesper Nilsson <jespernilsson93@hotmail.se>
2019-10-08 14:38:20 +0000

    add pop_if_full

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,3 +1,6 @@
+from queue import Full
+
+
 def scale_bounding_box(image_size, bounding_box, target_size):
     """Scales a bounding box to target image size"""
     x1p = bounding_box[0] / image_size[0]
@@ -10,3 +13,12 @@ def scale_bounding_box(image_size, bounding_box, target_size):
         x2p * target_size[0],
         y2p * target_size[1],
     )
+
+
+def pop_if_full(queue, item):
+    """If queue is full, pop oldest item and put the new item"""
+    try:
+        queue.put_nowait(item)
+    except Full:
+        queue.get()
+        queue.put_nowait(item)
