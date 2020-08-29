eabbcd58fe6f0e31bcb6bda2f9ac3a3d8ed42f07
Jesper Nilsson <jespernilsson93@hotmail.se>
2020-08-29 10:06:50 +0000

    fix vaapi codec and hwaccel args

--- src/const.py
+++ src/const.py
@@ -50,11 +50,24 @@ ENV_RASPBERRYPI3 = "VISERON_RASPBERRYPI3"
 
 HWACCEL_VAAPI = ["-hwaccel", "vaapi", "-vaapi_device", "/dev/dri/renderD128"]
 HWACCEL_VAAPI_ENCODER_FILTER = ["-vf", "format=nv12|vaapi,hwupload"]
-HWACCEL_VAAPI_ENCODER_CODEC = ["-vf", "format=nv12|vaapi,hwupload"]
-HWACCEL_CUDA_DECODER_CODEC = ["-c:v", "h264_cuvid"]
-HWACCEL_CUDA_ENCODER_CODEC = ["-c:v", "h264_nvenc"]
-HWACCEL_RPI3_DECODER_CODEC = ["-c:v", "h264_mmal"]
-HWACCEL_RPI3_ENCODER_CODEC = ["-c:v", "h264_omx"]
+HWACCEL_VAAPI_ENCODER_CODEC = ["h264_vaapi"]
+HWACCEL_CUDA_DECODER_CODEC = ["h264_cuvid"]
+HWACCEL_CUDA_ENCODER_CODEC = ["h264_nvenc"]
+HWACCEL_RPI3_DECODER_CODEC = ["h264_mmal"]
+HWACCEL_RPI3_ENCODER_CODEC = ["h264_omx"]
 
 RECORDER_GLOBAL_ARGS = ["-hide_banner", "-loglevel", "panic"]
 RECORDER_HWACCEL_ARGS = []
+
+DARKNET_DEFAULTS = {
+    "type": "darknet",
+    "model_path": "/detectors/models/darknet/yolo.weights",
+    "model_config": "/detectors/models/darknet/yolo.cfg",
+    "label_path": "/detectors/models/darknet/coco.names",
+}
+
+EDGETPU_DEFAULTS = {
+    "type": "edgetpu",
+    "model_path": "/detectors/models/edgetpu/model.tflite",
+    "label_path": "/detectors/models/edgetpu/labels.txt",
+}
