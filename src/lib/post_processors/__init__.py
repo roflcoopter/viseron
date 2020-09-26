8324bf7b8f87f5810f6230b055f71e9f6b9b36fa
Jesper Nilsson <jespernilsson93@hotmail.se>
2020-09-26 13:16:14 +0000

    validate config for dlib

--- src/lib/post_processors/__init__.py
+++ src/lib/post_processors/__init__.py
@@ -3,6 +3,8 @@ import logging
 from queue import Queue
 from threading import Thread
 
+from lib.config.config_logging import LoggingConfig
+
 LOGGER = logging.getLogger(__name__)
 
 
@@ -10,8 +12,9 @@ class PostProcessor:
     def __init__(self, processor_type, processor_config):
         LOGGER.debug(f"Initializing post processor {processor_type}")
         processor = self.import_processor(processor_type, processor_config)
+        config = processor.Config(processor.SCHEMA(processor_config))
         self.input_queue = Queue(maxsize=10)
-        self._post_processor = processor.Processor()
+        self._post_processor = processor.Processor(config)
 
         processor_thread = Thread(target=self.post_process)
         processor_thread.daemon = True
@@ -27,3 +30,17 @@ class PostProcessor:
     def post_process(self):
         while True:
             self._post_processor.process(self.input_queue.get())
+
+
+class PostProcessorConfig:
+    """Base config class for all post processors.
+    Each post processor has to have a Config class which inherits this class"""
+
+    def __init__(self, post_processor):
+        self._logging = None
+        if post_processor.get("logging", None):
+            self._logging = LoggingConfig(post_processor["logging"])
+
+    @property
+    def logging(self):
+        return self._logging
--- src/lib/post_processors/face_recognition/dlib/__init__.py
+++ src/lib/post_processors/face_recognition/dlib/__init__.py
@@ -1,11 +1,30 @@
 import logging
 
+from voluptuous import Optional
+
+from lib.post_processors import PostProcessorConfig
+from lib.post_processors.schema import SCHEMA as BASE_SCHEMA
+
+from .defaults import MODEL_PATH
+
+SCHEMA = BASE_SCHEMA.extend({Optional("model_path", default=MODEL_PATH): str,})
+
 LOGGER = logging.getLogger(__name__)
 
 
 class Processor:
-    def __init__(self):
+    def __init__(self, config):
         LOGGER.debug("Initializing dlib")
 
     def process(self, frame):
         LOGGER.debug(f"Processing {frame}")
+
+
+class Config(PostProcessorConfig):
+    def __init__(self, processor_config):
+        super().__init__(processor_config)
+        self._model_path = processor_config["model_path"]
+
+    @property
+    def model_path(self):
+        return self._model_path
