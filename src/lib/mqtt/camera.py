8c39448b9bdc9a020c9784ec7bc814bd613cda99
Jesper Nilsson <jespernilsson93@hotmail.se>
2020-10-03 09:45:47 +0000

    camera to device registry

--- src/lib/mqtt/camera.py
+++ src/lib/mqtt/camera.py
@@ -9,13 +9,13 @@ class MQTTCamera:
     def __init__(self, config, mqtt_queue):
         self._config = config
         self._mqtt_queue = mqtt_queue
+        self._name = config.camera.mqtt_name
+        self._device_name = f"{config.mqtt.client_id} {config.camera.name}"
+        self._unique_id = self.name
 
     @property
     def base_topic(self):
-        return (
-            f"{self._config.mqtt.discovery_prefix}/camera/"
-            f"{self._config.camera.mqtt_name}"
-        )
+        return f"{self._config.mqtt.discovery_prefix}/camera/" f"{self.name}"
 
     @property
     def state_topic(self):
@@ -26,13 +26,35 @@ class MQTTCamera:
         return f"{self.base_topic}/config"
 
     @property
+    def name(self):
+        return self._name
+
+    @property
+    def device_name(self):
+        return self._device_name
+
+    @property
+    def unique_id(self):
+        return self._unique_id
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": [self.device_name],
+            "name": self.device_name,
+            "manufacturer": "Viseron",
+        }
+
+    @property
     def config_payload(self):
         payload = {}
-        payload["name"] = self._config.camera.mqtt_name
+        payload["name"] = self.name
+        payload["unique_id"] = self.unique_id
         payload["topic"] = self.state_topic
         payload["availability_topic"] = self._config.mqtt.last_will_topic
         payload["payload_available"] = "alive"
         payload["payload_not_available"] = "dead"
+        payload["device"] = self.device_info
         return json.dumps(payload, indent=3)
 
     def on_connect(self, client):
