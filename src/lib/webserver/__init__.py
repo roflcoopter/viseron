29ceb9e5dae09e91ce386442410e16d7eb6f34b5
Jesper Nilsson <jespernilsson93@hotmail.se>
2020-11-15 09:31:51 +0000

    initial implementation of tornado

---
+++ src/lib/webserver/__init__.py
@@ -0,0 +1,69 @@
+import base64
+import datetime
+import threading
+import time
+
+import tornado.gen
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+from lib.webserver.stream_handler import StreamHandler
+
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    @tornado.gen.coroutine
+    def get_data(self):
+        while True:
+            print("Write time")
+            try:
+                yield self.write_message(
+                    {
+                        "current_time": datetime.datetime.strftime(
+                            datetime.datetime.now(), "%Y-%m-%d %H:%M:%S"
+                        )
+                    }
+                )
+                yield tornado.gen.sleep(1)
+            except tornado.websocket.WebSocketClosedError:
+                break
+
+    def open(self):
+        print("WebSocket opened")
+        tornado.ioloop.IOLoop.current().add_future(
+            self.get_data(), lambda f: self.close()
+        )
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+
+class RegularSocketHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("assets/index.html")
+
+
+class WebServer(threading.Thread):
+    def __init__(self):
+        super(WebServer, self).__init__(name="WebServer")
+        self.application = self.create_application()
+        self.application.listen(8888)
+        self.ioloop = tornado.ioloop.IOLoop.current()
+
+    def create_application(self):
+        return tornado.web.Application(
+            [
+                (r"/(?P<camera>[A-Za-z0-9_]+)/stream", StreamHandler),
+                (r"/ws-stream", RegularSocketHandler),
+                (r"/websocket", WebSocketHandler),
+            ],
+            debug=True,
+        )
+
+    def run(self):
+        self.ioloop.start()
