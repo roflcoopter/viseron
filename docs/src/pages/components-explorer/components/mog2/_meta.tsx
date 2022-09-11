d86d2139fe56b1282ca37bac003d96f4308abf09
Jesper Nilsson <jespernilsson93@hotmail.se>
2022-09-11 15:02:54 +0200

    add mog2 docs

---
+++ docs/src/pages/components-explorer/components/mog2/_meta.tsx
@@ -0,0 +1,12 @@
+import { Component } from "@site/src/types";
+
+const ComponentMetadata: Component = {
+  title: "MOG2",
+  name: "mog2",
+  description:
+    "MOG2 is a Gaussian Mixture-based Background/Foreground Segmentation Algorithm that provides motion detection capabilities.",
+  image: "https://brands.home-assistant.io/opencv/logo.png",
+  tags: ["motion_detector"],
+};
+
+export default ComponentMetadata;
--- viseron/components/mog2/__init__.py
+++ viseron/components/mog2/__init__.py
@@ -4,7 +4,8 @@ import voluptuous as vol
 from viseron import Viseron
 from viseron.domains import RequireDomain, setup_domain
 from viseron.domains.motion_detector import CAMERA_SCHEMA_SCANNER, CONFIG_CAMERAS
-from viseron.helpers.validators import none_to_dict
+from viseron.domains.motion_detector.const import DESC_CAMERAS
+from viseron.helpers.validators import CameraIdentifier, CoerceNoneToDict
 
 from .const import (
     COMPONENT,
@@ -17,38 +18,45 @@ from .const import (
     DEFAULT_HISTORY,
     DEFAULT_LEARNING_RATE,
     DEFAULT_THRESHOLD,
+    DESC_COMPONENT,
+    DESC_DETECT_SHADOWS,
+    DESC_HISTORY,
+    DESC_LEARNING_RATE,
+    DESC_MOTION_DETECTOR,
+    DESC_THRESHOLD,
 )
 
 CAMERA_SCHEMA = CAMERA_SCHEMA_SCANNER.extend(
     {
-        vol.Optional(CONFIG_THRESHOLD, default=DEFAULT_THRESHOLD): vol.All(
-            int, vol.Range(min=0, max=255)
-        ),
-        vol.Optional(CONFIG_HISTORY, default=DEFAULT_HISTORY): int,
-        vol.Optional(CONFIG_DETECT_SHADOWS, default=DEFAULT_DETECT_SHADOWS): bool,
-        vol.Optional(CONFIG_LEARNING_RATE, default=DEFAULT_LEARNING_RATE): vol.All(
-            vol.Any(
-                vol.All(
-                    float,
-                    vol.Range(min=0.0, max=1.0),
-                ),
-                1,
-                0,
-                -1,
-            ),
-            vol.Coerce(float),
-        ),
+        vol.Optional(
+            CONFIG_THRESHOLD, default=DEFAULT_THRESHOLD, description=DESC_THRESHOLD
+        ): vol.All(int, vol.Range(min=0, max=255)),
+        vol.Optional(
+            CONFIG_HISTORY, default=DEFAULT_HISTORY, description=DESC_HISTORY
+        ): int,
+        vol.Optional(
+            CONFIG_DETECT_SHADOWS,
+            default=DEFAULT_DETECT_SHADOWS,
+            description=DESC_DETECT_SHADOWS,
+        ): bool,
+        vol.Optional(
+            CONFIG_LEARNING_RATE,
+            default=DEFAULT_LEARNING_RATE,
+            description=DESC_LEARNING_RATE,
+        ): vol.All(vol.Coerce(float), vol.Range(min=-1, max=1.0)),
     }
 )
 
 
 CONFIG_SCHEMA = vol.Schema(
     {
-        COMPONENT: vol.Schema(
+        vol.Required(COMPONENT, description=DESC_COMPONENT): vol.Schema(
             {
-                vol.Required(CONFIG_MOTION_DETECTOR): {
-                    vol.Required(CONFIG_CAMERAS): {
-                        str: vol.All(none_to_dict, CAMERA_SCHEMA)
+                vol.Required(
+                    CONFIG_MOTION_DETECTOR, description=DESC_MOTION_DETECTOR
+                ): {
+                    vol.Required(CONFIG_CAMERAS, description=DESC_CAMERAS): {
+                        CameraIdentifier(): vol.All(CoerceNoneToDict(), CAMERA_SCHEMA),
                     },
                 },
             }
