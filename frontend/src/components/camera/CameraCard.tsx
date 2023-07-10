8c32278fa4d51dbb31df658c2ec740004c1c6f4e
Jesper Nilsson <jespernilsson93@hotmail.se>
2023-07-10 14:03:21 +0000

    switch back to @jy95/material-ui-image

--- frontend/src/components/camera/CameraCard.tsx
+++ frontend/src/components/camera/CameraCard.tsx
@@ -4,7 +4,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { useTheme } from "@mui/material/styles";
-import Image from "@roflcoopter/material-ui-image";
+import Image from "@jy95/material-ui-image";
 import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { usePageVisibility } from "react-page-visibility";
 
