4ad64a2dd3432fabb7d2f13555ee398ac89db9de
Jesper Nilsson <jespernilsson93@hotmail.se>
2023-07-17 19:14:24 +0000

    raise ComponentNotReady if darknet setup fails

--- viseron/components/darknet/__init__.py
+++ viseron/components/darknet/__init__.py
@@ -21,7 +21,7 @@ from viseron.domains.motion_detector.const import DOMAIN as MOTION_DETECTOR_DOMA
 from viseron.domains.object_detector import BASE_CONFIG_SCHEMA
 from viseron.domains.object_detector.const import CONFIG_CAMERAS
 from viseron.domains.object_detector.detected_object import DetectedObject
-from viseron.exceptions import ViseronError
+from viseron.exceptions import ComponentNotReady, ViseronError
 from viseron.helpers import letterbox_resize, pop_if_full
 from viseron.helpers.child_process_worker import ChildProcessWorker
 from viseron.helpers.logs import CTypesLogPipe
@@ -124,7 +124,10 @@ def setup(vis: Viseron, config: dict[str, Any]) -> bool:
         and config[CONFIG_OBJECT_DETECTOR][CONFIG_DNN_BACKEND] is None
         and config[CONFIG_OBJECT_DETECTOR][CONFIG_DNN_TARGET] is None
     ):
-        vis.data[COMPONENT] = DarknetNative(vis, config[CONFIG_OBJECT_DETECTOR])
+        try:
+            vis.data[COMPONENT] = DarknetNative(vis, config[CONFIG_OBJECT_DETECTOR])
+        except LoadDarknetError as error:
+            raise ComponentNotReady from error
     else:
         vis.data[COMPONENT] = DarknetDNN(vis, config[CONFIG_OBJECT_DETECTOR])
 
