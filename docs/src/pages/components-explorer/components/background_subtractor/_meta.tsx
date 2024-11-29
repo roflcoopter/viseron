12d11c0f6076e3067fd78cce18a33b6c2a098d41
Jesper Lodin <jespernilsson93@hotmail.se>
2024-11-29 11:20:00 +0000

    fix logo links

--- docs/src/pages/components-explorer/components/background_subtractor/_meta.tsx
+++ docs/src/pages/components-explorer/components/background_subtractor/_meta.tsx
@@ -5,7 +5,7 @@ const ComponentMetadata: Component = {
   name: "background_subtractor",
   description:
     "The background_subtractor component utilizes a set of OpenCV functions to implement motion detection using an algorithm called background subtraction.",
-  image: "https://brands.home-assistant.io/opencv/logo.png",
+  image: "https://github.com/opencv/opencv/wiki/logo/OpenCV_logo_no_text.png",
   tags: ["motion_detector"],
 };
 
