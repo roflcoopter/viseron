ae3d6decdb1fa9b854f138e0a368fcce72e87761
Jesper Lodin <jespernilsson93@hotmail.se>
2024-12-21 23:36:47 +0100

    lazy import of api endpoints

--- viseron/components/webserver/api/__init__.py
+++ viseron/components/webserver/api/__init__.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 
 import importlib
 import logging
+from functools import cache
+from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
 import tornado.routing
@@ -16,6 +18,33 @@ if TYPE_CHECKING:
     from viseron import Viseron
 
 LOGGER = logging.getLogger(__name__)
+
+
+@cache
+def get_handler(api_version: str, endpoint: str):
+    """Get handler for endpoint."""
+    version_path = Path(__file__).parent / api_version
+
+    if not version_path.is_dir():
+        return APINotFoundHandler
+
+    module_path = version_path / f"{endpoint}.py"
+    if not module_path.is_file():
+        return APINotFoundHandler
+
+    try:
+        module = importlib.import_module(
+            f"viseron.components.webserver.api.{api_version}.{endpoint}"
+        )
+        handler_name = f"{endpoint.title()}APIHandler"
+        if hasattr(module, handler_name):
+            return getattr(module, handler_name)
+    except ImportError as error:
+        LOGGER.warning(
+            f"Error importing API handler {endpoint}: {error}",
+            exc_info=True,
+        )
+    return APINotFoundHandler
 
 
 class APIRouter(tornado.routing.Router):
@@ -46,29 +75,12 @@ class APIRouter(tornado.routing.Router):
                 target_kwargs={"vis": self._vis},
             )
 
-        endpoint_handler = f"{endpoint.title()}APIHandler"
-        try:
-            handler = getattr(
-                importlib.import_module(
-                    f"viseron.components.webserver.api.{api_version}".format(
-                        api_version
-                    )
-                ),
-                endpoint_handler,
-            )
-        except AttributeError:
+        handler = get_handler(api_version, endpoint)
+        if handler == APINotFoundHandler:
             LOGGER.warning(
                 f"Unable to find handler for path: {request.path}",
-                exc_info=True,
             )
-            handler = APINotFoundHandler
-        except ModuleNotFoundError as error:
-            LOGGER.warning(
-                f"Error importing API endpoint module: {error}", exc_info=True
-            )
-            handler = APINotFoundHandler
 
-        # Return handler
         return self._application.get_handler_delegate(
             request=request,
             target_class=handler,
