f4b247c150390e2c9e66ddf68c55687ea95d0812
Jesper Lodin <jespernilsson93@hotmail.se>
2025-01-19 09:17:26 +0000

    add error boundaries to frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,8 +4,13 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { QueryClientProvider } from "@tanstack/react-query";
 import React, { Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 import { HashRouter as Router } from "react-router-dom";
 
+import {
+  ErrorBoundaryInner,
+  ErrorBoundaryOuter,
+} from "components/error/ErrorMessage";
 import { Loading } from "components/loading/Loading";
 import ToastContainer from "components/toast/ToastContainer";
 import { AuthProvider } from "context/AuthContext";
@@ -25,20 +30,24 @@ const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <ColorModeProvider>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <CssBaseline enableColorScheme />
-        <QueryClientProvider client={queryClient}>
-          <Router>
-            <AuthProvider>
-              <Suspense fallback={<Loading text="Loading chunk" />}>
-                <App />
-              </Suspense>
-            </AuthProvider>
-          </Router>
-          <ToastContainer />
-        </QueryClientProvider>
-      </LocalizationProvider>
-    </ColorModeProvider>
+    <ErrorBoundary FallbackComponent={ErrorBoundaryOuter}>
+      <ColorModeProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <CssBaseline enableColorScheme />
+          <QueryClientProvider client={queryClient}>
+            <Router>
+              <ErrorBoundary FallbackComponent={ErrorBoundaryInner}>
+                <AuthProvider>
+                  <Suspense fallback={<Loading text="Loading chunk" />}>
+                    <App />
+                  </Suspense>
+                </AuthProvider>
+              </ErrorBoundary>
+            </Router>
+            <ToastContainer />
+          </QueryClientProvider>
+        </LocalizationProvider>
+      </ColorModeProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
