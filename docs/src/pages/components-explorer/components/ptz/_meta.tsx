b332c644c6ee3729f0404ffe5fe9f66a64193bd9
smelis <621584+smelis@users.noreply.github.com>
2025-03-09 21:39:55 +0000

    baby don't like empty tags?

--- docs/src/pages/components-explorer/components/ptz/_meta.tsx
+++ docs/src/pages/components-explorer/components/ptz/_meta.tsx
@@ -5,7 +5,7 @@ const ComponentMetadata: Component = {
   name: "ptz",
   description: "Use this component to control PTZ cameras via Telegram.",
   image: "https://27bslash6.com/images/SPIDER1.jpg",
-  tags: [],
+  tags: ["camera"],
 };
 
 export default ComponentMetadata;
