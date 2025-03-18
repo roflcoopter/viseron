019fa8a8161b8d614b0fe59dc8535e82b1e3f8f0
Thibaut <vqzcajqm@c141.fr>
2025-03-18 01:55:20 +0000

    Add gotify component

---
+++ docs/src/pages/components-explorer/components/gotify/_meta.tsx
@@ -0,0 +1,12 @@
+import { Component } from "@site/src/types";
+
+const ComponentMetadata: Component = {
+  title: "<ENTER VALUE>",
+  name: "gotify",
+  description: "<ENTER VALUE>",
+  image: "<ENTER VALUE>",
+  tags: [],
+};
+
+export default ComponentMetadata;
+
