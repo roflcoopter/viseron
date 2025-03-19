1d589872866ed127c4b2e68c23d91fd080aab5dc
Thibaut <vqzcajqm@c141.fr>
2025-03-19 22:37:07 +0000

    add Notification type

--- docs/src/pages/components-explorer/components/telegram/_meta.tsx
+++ docs/src/pages/components-explorer/components/telegram/_meta.tsx
@@ -6,7 +6,7 @@ const ComponentMetadata: Component = {
   description: "Control PTZ cameras and receive notifications via Telegram",
   image:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/150px-Telegram_2019_Logo.svg.png",
-  tags: [],
+  tags: ["notification"],
 };
 
 export default ComponentMetadata;
