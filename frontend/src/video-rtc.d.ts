7aada843d9bc08a16da64b685d594c0bc487bb0e
Jesper Lodin <jespernilsson93@hotmail.se>
2025-05-09 06:56:35 +0000

    include go2rtc video-rtc.js in build

---
+++ frontend/src/video-rtc.d.ts
@@ -0,0 +1,27 @@
+export class VideoRTC extends HTMLElement {
+  state: string;
+
+  pcState: number;
+
+  video: HTMLVideoElement | null;
+
+  src: string;
+
+  private _controls: boolean;
+
+  public get controls(): boolean {
+    return this._controls;
+  }
+
+  public set controls(value: boolean) {
+    this._controls = value;
+  }
+
+  onmessage: { stream?: (msg: { type: string; value: string }) => void };
+  oninit(): void;
+  onconnect(): any;
+  ondisconnect(): void;
+  onopen(): any;
+  onclose(): any;
+  onpcvideo(ev: Event): void;
+}
