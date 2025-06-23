75c2cf720b1e6f8c0d7470b20e8ae2be2ca160ed
john <4852923-john---@users.noreply.gitlab.com>
2025-06-23 09:43:09 -0500

    update component logo link

--- docs/src/pages/components-explorer/components/yolo/_meta.tsx
+++ docs/src/pages/components-explorer/components/yolo/_meta.tsx
@@ -4,7 +4,7 @@ const ComponentMetadata: Component = {
   title: "Ultralytics YOLO",
   name: "yolo",
   description: "Ultralytics YOLO supports a wide range of YOLO models, from early versions like YOLOv3 to the latest YOLO11",
-  image: "/img/Ultralytics_YOLO_Logomark_Original.svg",
+  image: "https://cdn.prod.website-files.com/680a070c3b99253410dd3dcf/680a070c3b99253410dd3e88_UltralyticsYOLO_full_blue.svg",
   tags: ['object_detector'],
 };
 
