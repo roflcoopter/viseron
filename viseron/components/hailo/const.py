0298c9c6c6233040c3934e77fb8dd896b3026113
Jesper Lodin <jespernilsson93@hotmail.se>
2025-10-09 13:10:26 +0000

    allow multiple processes to share hailo device

--- viseron/components/hailo/const.py
+++ viseron/components/hailo/const.py
@@ -15,20 +15,23 @@ HAILO8L_DEFAULT_URL = (
 
 # CONFIG_SCHEMA constants
 CONFIG_OBJECT_DETECTOR = "object_detector"
+CONFIG_MULTI_PROCESS_SERVICE = "multi_process_service"
+
+DEFAULT_MULTI_PROCESS_SERVICE: Final = True
+
+DESC_COMPONENT = "Hailo configuration."
+DESC_OBJECT_DETECTOR = "Object detector domain config."
+DESC_MULTI_PROCESS_SERVICE = "Allow multiple processes to share the Hailo device."
 
 # OBJECT_DETECTOR_SCHEMA constants
 CONFIG_MODEL_PATH = "model_path"
 CONFIG_LABEL_PATH = "label_path"
 CONFIG_MAX_DETECTIONS = "max_detections"
 
-
 DEFAULT_MODEL_PATH: Final = None
 DEFAULT_LABEL_PATH = "/detectors/models/darknet/coco.names"
 DEFAULT_MAX_DETECTIONS = 50
 
-
-DESC_COMPONENT = "Hailo configuration."
-DESC_OBJECT_DETECTOR = "Object detector domain config."
 DESC_MODEL_PATH = (
     "Path or URL to a Hailo-8 model in HEF format."
     " If a URL is provided, the model will be downloaded on startup."
